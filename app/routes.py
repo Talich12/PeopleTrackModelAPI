2680c4ce6edbf5db6af51e58b1a7b03a82928619
Talich12 <denis.tal@mail.ru>
2023-10-13 20:47:30 +1000

    refactor: Переименовать функцию предсказания

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 
 @app.route("/predict", methods=["POST"])
-def login():
+def predict():
     img = request.files['file'].read()  # read the file
     npimg = np.fromstring(img, np.uint8)  # convert to numpy array
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)  # convert numpy array to image
